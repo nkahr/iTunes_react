c0d937265516d6e34e1e13c0a315e9be4f7a9a30
Nina Kahr <ntkahr@gmail.com>
2017-03-08 09:00:23 +0000

    remove genre request

--- client/src/containers/SongsContainer.jsx
+++ client/src/containers/SongsContainer.jsx
@@ -16,27 +16,7 @@ var SongsContainer = React.createClass({
     }.bind(this);
 
     request.send();
-    // this.getGenres();
   },
-
-  // getGenres: function() {
-  //   var url = "https://itunes.apple.com/WebObjects/MZStoreServices.woa/ws/genres";
-  //   var request = new XMLHttpRequest();
-  //   request.open('get', url);
-
-  //   request.onload = function(){
-  //     console.log(request.status);
-  //     console.log(request.responseText);
-  //     if (request.status !== 200) return;
-  //     var data = JSON.parse(request.responseText);
-  //     console.log("data", data);
-  //     var genreArray = data["34"]["subgenres"];
-  //     // console.log(genreArray);
-  //     this.setState({genres: genreArray});
-  //   }.bind(this);
-
-  //   request.send();
-  // },
 
   getInitialState: function () {
     return { songs: [], genres: []};
